8319dab132852f3e972b30fbaf7611c3b031c66c
TheMan-txt <77545089+TheMan-txt@users.noreply.github.com>
2022-10-12 16:04:52 -0400

    First Project
    We will work on this as our first 2D game.

---
+++ Boss Battle/Assets/PlayerMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterMovement controller;// Referrencing CharacterMovement script. 
+    //Note variables have to be used with an existing component or they will create an error.
+    float horizontalMove = 0f;
+    public float runSpeed = 40f;
+    float verticalMove = 0f;
+    bool jump = false;
+  
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+          
+        }
+        else if (Input.GetButtonUp("Jump"))
+        {
+            jump = false;
+
+        }
+       horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+      
+    }
+    void FixedUpdate()
+    {//This is used for player movement because it is smoother.
+        
+        controller.Move(horizontalMove * Time.deltaTime, false, jump);
+
+    }
+}

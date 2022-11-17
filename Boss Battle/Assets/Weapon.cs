0baaacee7c6b73321d52dcf46f4ddb2ff5c1f2f0
TheMan-txt <77545089+TheMan-txt@users.noreply.github.com>
2022-11-17 07:28:59 -0500

    Revert "Clear out old stuff"
    This reverts commit 1b0d75342c40afaab5f15589d266f05cc277ce0f.

---
+++ Boss Battle/Assets/Weapon.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.F ))
+        {
+
+            Attack();
+
+        }
+
+        void Attack()
+        {
+
+
+
+        }
+
+
+    }
+
+
+}
